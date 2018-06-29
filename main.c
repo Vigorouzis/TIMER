d0adfc5734dac69a097e37375a93a0d50bc219bc
Nikita Kanunov <Nkanunov@gmail.com>
2018-06-29 15:24:53 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stm32f10x_conf.h>
+
+
+void TIM4_IRQHandler(void)
+{
+        if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+        {
+            // Обязательно сбрасываем флаг
+            TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+            GPIOC->ODR ^= GPIO_Pin_13;
+        }
+}
+
+int main(void)
+{
+
+    /* Initialize LED which connected to PC13 */
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    // Enable PORTC Clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    /* Configure the GPIO_LED pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+
+    // TIMER4
+    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+    TIM_TimeBaseStructInit(&TIMER_InitStructure);
+    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIMER_InitStructure.TIM_Prescaler =8000;
+    TIMER_InitStructure.TIM_Period = 500;
+    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM4, ENABLE);
+
+    /* NVIC Configuration */
+    /* Enable the TIM4_IRQn Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    while(1)
+    {
+    }
+}
